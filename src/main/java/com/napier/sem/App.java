d87dce486585101356e4db04c58cc888fcae0090
Lewis Watson <lewiswatson55@protonmail.com>
2021-02-03 16:25:27 +0000

    Comment to trigger travis

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,6 +9,7 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Comment to trigger travis
         // Connect to MongoDB on local system - we're using port 27000
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
