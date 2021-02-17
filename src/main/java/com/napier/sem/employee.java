0eed981559aca949518f1dfb889bbf5954dcdd08
Lewis Watson <lewiswatson55@protonmail.com>
2021-02-17 12:25:35 +0000

    Employee and running SQL - lab 3

---
+++ src/main/java/com/napier/sem/employee.java
@@ -0,0 +1,46 @@
+package com.napier.sem;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Represents an employee
+ */
+public class employee
+{
+    /**
+     * Employee number
+     */
+    public int emp_no;
+
+    /**
+     * Employee's first name
+     */
+    public String first_name;
+
+    /**
+     * Employee's last name
+     */
+    public String last_name;
+
+    /**
+     * Employee's job title
+     */
+    public String title;
+
+    /**
+     * Employee's salary
+     */
+    public int salary;
+
+    /**
+     * Employee's current department
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public String manager;
+
+}
